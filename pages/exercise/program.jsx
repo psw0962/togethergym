526b2acdc7afc60eaba4cd89a125fe3950d065f5
psw <psw0962@naver.com>
2023-05-27 22:16:38 +0900

    fix: 빌드 에러 수정

--- pages/exercise/program.jsx
+++ pages/exercise/program.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import useLocalStorage from 'node_modules/use-local-storage/dist/index';
 import { useRecoilState } from 'recoil';
 import { timerMethodStateAtom } from 'atoms/index';
@@ -8,12 +8,14 @@ import EightTimer from '@/component/exercise/eight/EightTimer';
 import CustomTimer from '@/component/exercise/custom/CustomTimer';
 
 const Program = () => {
-  const audio = new Audio('/sounds/beep.mp3');
+  const [audio, setAudio] = useState();
   const [timerMethod, setTimerMethod] = useRecoilState(timerMethodStateAtom);
   const [flag, setFlag] = useLocalStorage('flag', {});
 
   // 최초 셋팅 이펙트
   useEffect(() => {
+    setAudio(new Audio('/sounds/beep.mp3'));
+
     // 타이머에 따라 첫 셋팅 분기
     if (timerMethod === 'basic') {
       setFlag({
