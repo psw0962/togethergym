f946bf948283582b2bd93c2c651c5408a3ab2332
psw0962 <psw0962@naver.com>
2024-01-27 13:32:03 +0900

    working

--- next.config.js
+++ next.config.js
@@ -1,27 +1,34 @@
 /** @type {import('next').NextConfig} */
-// const withInterceptStdout = require('next-intercept-stdout');
+const withInterceptStdout = require('next-intercept-stdout');
 
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
   siteUrl: 'https://www.togethergym.co.kr',
-  generateRobotsTxt: true,
-  // images: {
-  //   domains: [],
-  // },
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.mp3$/,
-  //       use: {
-  //         loader: 'file-loader',
-  //       },
-  //     },
-  //   ],
-  // },
+  images: {
+    domains: [],
+  },
 };
 
-// module.exports = withInterceptStdout(nextConfig, text =>
-//   text.includes('Duplicate atom key') ? '' : text,
-// );
+module.exports = withInterceptStdout(nextConfig, text =>
+  text.includes('Duplicate atom key') ? '' : text,
+);
 
-module.exports = nextConfig;
+// 웹팩 설정 추가
+module.exports = withInterceptStdout(
+  {
+    reactStrictMode: true,
+    images: {
+      domains: [],
+    },
+    webpack(config, options) {
+      config.module.rules.push({
+        test: /\.mp3$/,
+        use: {
+          loader: 'file-loader',
+        },
+      });
+      return config;
+    },
+  },
+  text => (text.includes('Duplicate atom key') ? '' : text),
+);
